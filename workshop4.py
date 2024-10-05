8c3e3fdb37fe6b55ccd33fef019cc190c9d01b81
Shad Spreiter <loyaldogtech@gmail.com>
2024-10-04 18:24:45 -0700

    task 2 completed

--- workshop4.py
+++ workshop4.py
@@ -4,6 +4,14 @@ class User:
         self.pin = pin
         self.password = password
 
+    def change_name(self, name):
+        self.name = name
+
+    def change_pin(self, pin):
+        self.pin = pin
+
+    def change_password(self, password):
+        self.password = password
 
 
 
@@ -13,8 +21,13 @@ class User:
 
 
 
-
-""" Driver Code for Task 1 """
+# Driver Code for Task 1 
 user = User("Bob", 1234, "password")
+print(user.name, user.pin, user.password)
+
+# Driver Code for Task 2
+user.change_name("Rob")
+user.change_pin(4321)
+user.change_password("rob1234")
 print(user.name, user.pin, user.password)
 
