1f8229443610311e9dd589027d92499859990cd4
Shad Spreiter <loyaldogtech@gmail.com>
2024-10-04 18:29:43 -0700

    task 3 completed

--- workshop4.py
+++ workshop4.py
@@ -13,6 +13,11 @@ class User:
     def change_password(self, password):
         self.password = password
 
+class BankUser(User):
+    def __init__(self, name, pin, password, balance=0):
+        super().__init__(name, pin, password)
+        self.balance = balance
+
 
 
 
@@ -22,12 +27,16 @@ class User:
 
 
 # Driver Code for Task 1 
-user = User("Bob", 1234, "password")
-print(user.name, user.pin, user.password)
+# user = User("Bob", 1234, "password")
+# print(user.name, user.pin, user.password)
 
-# Driver Code for Task 2
-user.change_name("Rob")
-user.change_pin(4321)
-user.change_password("rob1234")
-print(user.name, user.pin, user.password)
+# # Driver Code for Task 2
+# user.change_name("Rob")
+# user.change_pin(4321)
+# user.change_password("rob1234")
+# print(user.name, user.pin, user.password)
+
+# Driver Code for Task 3
+bank_user = BankUser("Bob", 1234, "password", 0)
+print(bank_user.name, bank_user.pin, bank_user.password, bank_user.balance)
 
